0ce4dbe297d6810a22911067d9782c0f3d0ff74e
r1ttle <yurittle0327@gmail.com>
2021-08-20 15:50:53 +0900

    first commit

--- system.js
+++ system.js
@@ -18,7 +18,7 @@ function enterkey(element) {
 
     $.ajax({
         method: "GET",
-        url: "https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + nickname + "?api_key=RGAPI-5c96ae2e-82d4-4c7e-b7ca-329fd661ef0b",
+        url: "https://cors0327.herokuapp.com/https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + nickname + "?api_key=RGAPI-5c96ae2e-82d4-4c7e-b7ca-329fd661ef0b",
         data: { summonerName: nickname },
         
         headers: {
@@ -35,7 +35,7 @@ function enterkey(element) {
 
             $.ajax({
                 method: "GET",
-                url: "https://kr.api.riotgames.com/lol/match/v4/matchlists/by-account/" + msg.accountId + "?api_key=RGAPI-5c96ae2e-82d4-4c7e-b7ca-329fd661ef0b",
+                url: "https://cors0327.herokuapp.com/https://kr.api.riotgames.com/lol/match/v4/matchlists/by-account/" + msg.accountId + "?api_key=RGAPI-5c96ae2e-82d4-4c7e-b7ca-329fd661ef0b",
                 data: { encryptedAccountId: msg.accountId },
                 headers: {
                     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36",
@@ -55,7 +55,7 @@ function enterkey(element) {
 
                         $.ajax({
                             method: "GET",
-                            url: "https://kr.api.riotgames.com/lol/match/v4/matches/" + gameID[index] + "?api_key=RGAPI-5c96ae2e-82d4-4c7e-b7ca-329fd661ef0b",
+                            url: "https://cors0327.herokuapp.com/https://kr.api.riotgames.com/lol/match/v4/matches/" + gameID[index] + "?api_key=RGAPI-5c96ae2e-82d4-4c7e-b7ca-329fd661ef0b",
                             data: { matchId: gameID[index] },
                             headers: {
                                 "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36",
