c97bce5de76600a2d2b69dcb684dfb3021e7414b
r1ttle <yurittle0327@gmail.com>
2021-09-01 09:09:33 +0900

    first commit

--- system.js
+++ system.js
@@ -1,4 +1,3 @@
-
 function searchToggle(obj, evt) {
     var container = $(obj).closest('.search-wrapper');
     if (!container.hasClass('active')) {
@@ -7,36 +6,51 @@ function searchToggle(obj, evt) {
     }
     else if (container.hasClass('active') && $(obj).closest('.input-holder').length == 0) {
         container.removeClass('active');
+        // clear input
         container.find('.search-input').val('');
     }
 }
 
+function myfunc() {
+    location.href = "search.html";
+}
+
+function setNickname(elem){
+
+    var name = elem;
+    window.localStorage.setItem('name', name);
+    console.log(window.localStorage.getItem('name'));
+}
+
+function loadNickname(){
+    return window.localStorage.getItem('name');
+}
 
 function enterkey(element) {
 
-    var nickname = element.value;
+    var nickname = element;
 
     $.ajax({
         method: "GET",
         url: "https://cors0327.herokuapp.com/https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + nickname + "?api_key=RGAPI-5c96ae2e-82d4-4c7e-b7ca-329fd661ef0b",
         data: { summonerName: nickname },
-        
+
         headers: {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36",
             "Accept-Language": "ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7",
             "Accept-Charset": "application/x-www-form-urlencoded; charset=UTF-8",
             "Origin": "https://developer.riotgames.com"
-        }
-    })
-        .done(function (msg) {
+        },
+        success: function (result) {
 
             alert("success!");
-            console.log(msg.accountId);
+
+            console.log(result)
 
             $.ajax({
                 method: "GET",
-                url: "https://cors0327.herokuapp.com/https://kr.api.riotgames.com/lol/match/v4/matchlists/by-account/" + msg.accountId + "?api_key=RGAPI-5c96ae2e-82d4-4c7e-b7ca-329fd661ef0b",
-                data: { encryptedAccountId: msg.accountId },
+                url: "https://cors0327.herokuapp.com/https://kr.api.riotgames.com/lol/match/v4/matchlists/by-account/" + result.accountId + "?api_key=RGAPI-5c96ae2e-82d4-4c7e-b7ca-329fd661ef0b",
+                data: { encryptedAccountId: result.accountId },
                 headers: {
                     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36",
                     "Accept-Language": "ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7",
@@ -49,9 +63,8 @@ function enterkey(element) {
                     var index;
                     var gameID = [];
 
-                    for (index = 0; index < 10; index++) {
+                    for (index = 0; index < 50; index++) {
                         gameID[index] = msg.matches[index].gameId
-                        console.log(gameID[index])
 
                         $.ajax({
                             method: "GET",
@@ -65,10 +78,72 @@ function enterkey(element) {
                             }
                         })
                             .done(function (msg) {
+
                                 console.log(msg);
+
+                                var KDA;
+                                var myTeam;
+                                var myKDA = 0;
+                                var otherKDA = 0;
+
+                                var mySkill;
+                                var teamSkill;
+
+                                for (index = 0; index < 10; index++) {
+                                    if (msg.participantIdentities[index].player.summonerName == nickname) {
+                                        if (parseFloat(((msg.participants[index].stats.kills + msg.participants[index].stats.assists) / msg.participants[index].stats.deaths)).toFixed(2) == Infinity) {
+                                            myKDA = parseFloat(((msg.participants[index].stats.kills + msg.participants[index].stats.assists)).toFixed(2));
+                                        }
+                                        else {
+                                            myKDA = parseFloat(((msg.participants[index].stats.kills + msg.participants[index].stats.assists) / msg.participants[index].stats.deaths).toFixed(2));
+                                        }
+                                        myTeam = msg.participants[index].teamId;
+                                    }
+                                    KDA = ((msg.participants[index].stats.kills + msg.participants[index].stats.assists) / msg.participants[index].stats.deaths).toFixed(2);
+                                    console.log(msg.participantIdentities[index].player.summonerName + ",  kda:" + KDA);
+                                }
+
+
+                                for (index = 0; index < 10; index++) {
+                                    if (msg.participants[index].teamId == myTeam && msg.participantIdentities[index].player.summonerName != nickname) {
+
+                                        if (parseFloat(((msg.participants[index].stats.kills + msg.participants[index].stats.assists) / msg.participants[index].stats.deaths)).toFixed(2) == Infinity) {
+                                            otherKDA += parseFloat((msg.participants[index].stats.kills + msg.participants[index].stats.assists).toFixed(2));
+                                        }
+                                        else {
+                                            otherKDA += parseFloat(((msg.participants[index].stats.kills + msg.participants[index].stats.assists) / msg.participants[index].stats.deaths).toFixed(2));
+                                        }
+                                    }
+                                }
+
+                                otherKDA /= 4;
+
+                                console.log("나의 KDA : " + myKDA);
+                                console.log("팀원의 KDA : " + otherKDA);
+
+                                if(otherKDA < 1){
+                                    teamSkill = "인력거";
+                                }
+                                else if(1 <= otherKDA && otherKDA < 2){
+                                    teamSkill = "마차";
+                                }
+                                else if(2 <= otherKDA && otherKDA <= 3){
+                                    teamSkill = "자가용";
+                                }
+                                else if(3 <= otherKDA && otherKDA <= 6){
+                                    teamSkill = "버스";
+                                }
+                                else if(otherKDA > 5){
+                                    teamSkill = "KTX";
+                                }
+
+                                console.log("팀운 : " + teamSkill);
                             })
                     }
                 });
-        });
-        
+        },
+        error: function (request, status, error) {
+            alert("존재하지 않는 소환사 이름입니다.");
+        }
+    })
 }
